2a2b7ec449a596bfa56a061b248964442e8785aa
kazuki.matsumura <kazuki_matsumura@icloud.com>
2019-11-17 12:41:30 +0000

    CRUD tasks

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>ToDoリスト</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>To Do</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+    {!! link_to_route('tasks.create', '新規リスト作成', [], ['class' => 'btn btn-primary']) !!}
+    {!! link_to_route('tasks.edit', 'このリストを編集', ['id' => $task->id], ['class' => 'btn btn-light']) !!}
+
+@endsection
